ee30f51dec7de179603fe3dd1e3e70e31db7b488
Demitrious Kelly <demitrious@automattic.com>
2016-06-09 10:48:25 -0700

    package docs

---
+++ desc.go
@@ -0,0 +1,8 @@
+// A library to help in dealing with WordPress.com guided transfers in Go
+//
+// This is meant to support hosting providers looking to support incoming
+// transfers via the Guided Transfer API.
+//
+// For end user documentation about this feature, please  See:
+// https://en.support.wordpress.com/guided-transfer/
+package gt
